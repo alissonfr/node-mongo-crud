837d57c5d5c9249b97007eba28bcc8c886b26b63
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2022-06-03 10:54:07 -0300

    habilitei o CORS

--- src/app.js
+++ src/app.js
@@ -8,22 +8,35 @@ const config = require('./config')
 const app = express();
 const router = express.Router();
 
-    //conecta ao banco
+//conecta ao banco
 mongoose.connect(config.connectionString);
 
-    //carrega os models
+//carrega os models
 const Product = require('./models/product')
 const Customer = require('./models/customer')
 const Order = require('./models/order')
 
-    //carrega as rotas
+//carrega as rotas
 const indexRoute = require('./routes/index-routes');
 const productRoute = require('./routes/product-route')
 const customerRoute = require('./routes/customer-route')
 const orderRoute = require('./routes/order-route')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Habilita o CORS (para conseguir fazer reqs de outras urls)
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
