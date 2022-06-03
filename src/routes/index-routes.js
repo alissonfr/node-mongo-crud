91c3d1d8baa6f9fc739f19f4066b5fe4cd96bc87
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2022-06-03 10:57:57 -0300

    API FINALIZADA

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2"
+        title: "Loja API - Node/MongoDB",
+        version: "1.0.0"
     });
 });
 
